700ea8b12921c607318e5b0ed52555ca5b30f884
little-pinecone <marta.szymek.mail@gmail.com>
2022-03-07 11:55:59 +0100

    feat: add example POST endpoint
    * enable csrf in Springdoc
    * update tests

--- src/main/java/in/keepgrowing/springboottypesafeuserroles/example/adapters/driving/api/controllers/ExampleController.java
+++ src/main/java/in/keepgrowing/springboottypesafeuserroles/example/adapters/driving/api/controllers/ExampleController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExampleController {
 
     @GetMapping
-    @Operation(summary = "Example endpoint")
+    @Operation(summary = "Example GET endpoint")
     public ResponseEntity<Void> get() {
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping
+    @Operation(summary = "Example POST endpoint")
+    public ResponseEntity<Void> post() {
         return ResponseEntity.ok().build();
     }
 }
--- src/test/java/in/keepgrowing/springboottypesafeuserroles/example/adapters/driving/api/controllers/ExampleControllerTest.java
+++ src/test/java/in/keepgrowing/springboottypesafeuserroles/example/adapters/driving/api/controllers/ExampleControllerTest.java
@@ -7,11 +7,15 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
 class ExampleControllerTest {
+
+    private static final String PATH = "/" + ExampleControllerPaths.EXAMPLE_PATH;
 
     @Autowired
     private ExampleController controller;
@@ -26,7 +30,14 @@ class ExampleControllerTest {
 
     @Test
     void shouldCallGetEndpoint() throws Exception {
-        mvc.perform(get("/example").contentType(MediaType.APPLICATION_JSON))
+        mvc.perform(get(PATH).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldCallPostEndpoint() throws Exception {
+        mvc.perform(post(PATH).contentType(MediaType.APPLICATION_JSON)
+                        .with(csrf()))
                 .andExpect(status().isOk());
     }
 }
