eb728a58d8fb3fced24c015b9cad3788fc49babf
little-pinecone <marta.szymek.mail@gmail.com>
2022-03-07 10:56:07 +0100

    feat: initial commit

---
+++ src/main/java/in/keepgrowing/springboottypesafeuserroles/SpringBootTypeSafeUserRolesApplication.java
@@ -0,0 +1,13 @@
+package in.keepgrowing.springboottypesafeuserroles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTypeSafeUserRolesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTypeSafeUserRolesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/keepgrowing/springboottypesafeuserroles/SpringBootTypeSafeUserRolesApplicationTests.java
@@ -0,0 +1,13 @@
+package in.keepgrowing.springboottypesafeuserroles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTypeSafeUserRolesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
